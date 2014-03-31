bcf0842628222b587043a3be24a3f09b0d1e6469
theRx <rx782forever@yahoo.com>
2014-03-31 12:53:47 -0700

    Mid-Sprint 10
    LUA and XML files set up
    Spin controls need to be re-implemented
    Proper creation of balls needs to be implemented.

--- Source/GameApplication/IController.cpp
+++ Source/GameApplication/IController.cpp
@@ -31,6 +31,7 @@ VisBaseEntity_cl* IController::AddRagdoll(){
 VisBaseEntity_cl* IController::AddSphere(){
 	VisBaseEntity_cl *ent = Vision::Game.CreateEntity("VisBaseEntity_cl", hkvVec3(-100.0f, -30, 100), "Models\\Misc\\Sphere.Model");
 	vHavokRigidBody *sphere = new vHavokRigidBody();
+	sphere->Havok_Mass = 0.1f;
 	sphere->Havok_TightFit = true;
 	sphere->Havok_Restitution = 1.0f;
 	ent->AddComponent(sphere);
